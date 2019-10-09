225ae8839057dcf7dec4d9865c7b0bc330cf5236
Romario Gustave <romagustave10@gmail.com>
2019-10-09 22:19:01 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,6 +14,6 @@ end
 def find_min_value(array)
   # Add your solution here
   array=["1","2","3","4","5"]
- min_value = ("1","2","3","4","5").to_a.method(:min)
+ min_value = ("1","2","3","4","5").to_a.min
   puts min_value
 end
